5be8f30eb5a79584206f0f9baabd45a9b2cfbcc8
Ashishpf <ashish.sondagar@pepperfry.com>
2018-09-21 18:47:28 +0530

    yii </> Fri, 2018-09-21, 18:47:28 IST 264

---
+++ gitPushU.bash
@@ -0,0 +1,17 @@
+echo "pingpong"
+
+git add *
+
+echo Write your commit: 
+
+read cmmt
+
+echo You commit: $cmmt
+
+git commit -m "$cmmt </> $(date +"%a, %Y-%m-%d, %H:%M:%S %Z %j")" 
+
+git pull remtecm brnecmo
+
+git push -f remtecm brnecmo 
+
+$SHELL
